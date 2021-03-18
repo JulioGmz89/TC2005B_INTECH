8725991a8405300633257eeec01f1abfe041a5d5
Luis-ERP <leramirezp@hotmail.com>
2021-03-18 00:21:25 -0600

    mysql connection

---
+++ Webapp/models/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+// require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: 'us-cdbr-east-03.cleardb.com', //process.env.DB_HOST,
+    user: 'b0ff57321e81f0', //process.env.DB_USER,
+    database: 'heroku_265358198a20512', //process.env.DB_NAME,
+    password: '6b5ad8b1'//process.env.DB_PASSWORD
+});
+
+module.exports = pool.promise();
