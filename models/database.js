c7e6075f4f3e33d1a279511d5be193ba8bf66fa5
Luis-ERP <leramirezp@hotmail.com>
2021-03-18 20:58:34 -0600

    new branch static

--- models/database.js
+++ models/database.js
@@ -1,11 +1,38 @@
 const mysql = require('mysql2');
 // require('dotenv').config();
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     host: 'us-cdbr-east-03.cleardb.com', //process.env.DB_HOST,
     user: 'b0ff57321e81f0', //process.env.DB_USER,
     database: 'heroku_265358198a20512', //process.env.DB_NAME,
     password: '6b5ad8b1'//process.env.DB_PASSWORD
 });
 
-module.exports = pool.promise();
+// const connection = pool.promise();
+module.exports = connection;
+
+
+function fetchUsuario(args={}){
+	let subquery = '';
+	const keys = Object.keys(args);
+	if (keys.length > 0){
+		subquery += 'where ';
+	}
+	keys.forEach( key => {
+		subquery += `${key} = ${JSON.stringify(args[key])} `;
+	});
+	const query = `select email_usuario, nombre_usuario from Usuario ${subquery};`;
+    let result = [];
+    connection.connect();
+    connection.query(query).then(function(err, rows, fields) {
+            if (err) console.log(err);
+            console.log(rows);
+            result = rows;
+            return result;
+        }
+    );
+    connection.end();
+    
+}
+
+console.log(fetchUsuario());
