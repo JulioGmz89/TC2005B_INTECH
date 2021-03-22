3fdf04c85448d7df4849f15d05cb1f05fd07bca2
Luis-ERP <leramirezp@hotmail.com>
2021-03-22 02:06:49 -0600

    intento4 proyectos controlador

--- controllers/proyectos_controller.js
+++ controllers/proyectos_controller.js
@@ -3,23 +3,24 @@ const models = require('../models/proyectos');
 async function getProyectos(request, response, next) {
     const email_usuario = 'Daniel@hotmail.com';
     const proyectos = await models.fetchProyectosUsuario(email_usuario);
-    const dataProyectos = [];
-    console.log(proyectos[0]);
-    proyectos[0].forEach( async proyecto => {
-        console.log(proyecto.id_proyecto);
+    var dataProyectos = [];
+    await proyectos[0].forEach( async proyecto => {
+        // console.log('1', dataProyectos.length);
         const integrantes = await models.fetchIntegrantesProyecto(proyecto.id_proyecto);
         const tareasCompl = await models.fetchTareasCompletadasProyecto(proyecto.id_proyecto);
         const tiempoEstim = await models.fetchTiempoEsProyecto(proyecto.id_proyecto);
         const data = {
-            idProyecto: id_proyecto,
-            nombreProyecto: nombre_proyecto,
-            nuTareasCompletadas: tareasCompl.length,
-            tareasCompletadas: tareasCompl,
-            tiempoEstimado: tiempoEstim,
-            integrantes: integrantes
+            idProyecto: proyecto.id_proyecto,
+            nombreProyecto: proyecto.nombre_proyecto,
+            nuTareasCompletadas: tareasCompl[0].length,
+            tiempoEstimado: tiempoEstim[0][0]['tiempo_estimado'],
+            integrantes: integrantes[0]
         };
+        console.log(dataProyectos.length);
         dataProyectos.push(data);
+        // console.log(dataProyectos.length);
     });
+    console.log('\n2',  dataProyectos);
     response.render('proyectos', {
         title: 'Puntos Agiles',
         proyectos: dataProyectos
