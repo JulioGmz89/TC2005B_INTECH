a2ffe3771a3b165a1e6b0c8b9159720f2c821550
Luis-ERP <leramirezp@hotmail.com>
2021-03-22 00:44:07 -0600

    added proyectos controller

--- controllers/proyectos_controller.js
+++ controllers/proyectos_controller.js
@@ -1,14 +1,25 @@
- /* const id = request.params.perro_id;
-    Perro.fetchOne(id)
-        .then(([rows, fieldData]) => {
-            response.render('perroIndividual', {
-                perros: rows,
-                titulo: 'Perro',
-                isLoggedIn: request.session.isLoggedIn === true ? true : false
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
- */
+const models = require('../models/proyectos');
 
+export async function getProyectos(request, response, next) {
+    const email_usuario = '';
+    const proyectos = await models.fetchProyectosUsuario(email_usuario);
+    const dataProyectos = [];
+    proyectos.forEach( proyecto => {
+        const integrantes = await fetchIntegrantesProyecto(proyecto.id_proyecto);
+        const tareasCompl = await fetchTareasCompletadasProyecto(proyecto.id_proyecto);
+        const tiempoEstim = await fetchTiempoEsProyecto(proyecto.id_proyecto);
+        const data = {
+            idProyecto: id_proyecto,
+            nombreProyecto: nombre_proyecto,
+            nuTareasCompletadas: tareasCompl.length,
+            tareasCompletadas: tareasCompl,
+            tiempoEstimado: tiempoEstim,
+            integrantes: integrantes
+        };
+        dataProyectos.push(data);
+    });
+    response.render('proyectos', {
+        title: 'Puntos Agiles',
+        proyectos: dataProyectos
+    });
+};
--- models/proyectos.js
+++ models/proyectos.js
@@ -1,22 +1,22 @@
 const db = require('./database');
 
 
-function fetchProyectosUsuario(email_usuario){
+export function fetchProyectosUsuario(email_usuario){
 	const query = `select  id_proyecto, nombre_proyecto, descripcion_proyecto, fechaInicio_proyecto from Proyecto P, Usuario_Proyecto UP where UP.email_usuario = '${email_usuario}' and UP.id_proyecto = P.proyecto;`;
 	return db.query(query);
 }
 
-function fetchIntegrantesProyecto(id_proyecto) {
-	const query = `select U.email_usuario, U.nombre_usuario from Usuario U, Usuario_Proyecto UP where UP.id_proyecto = '${id_proyecto}' and UP.email_usuario = U.email_usuario;`;
+export function fetchIntegrantesProyecto(id_proyecto) {
+	const query = `select UP.id_proyecto, U.email_usuario, U.nombre_usuario from Usuario U, Usuario_Proyecto UP where UP.id_proyecto = '${id_proyecto}' and UP.email_usuario = U.email_usuario;`;
 	return db.query(query);
 }
 
-function fetchTareasCompletadasProyecto(id_proyecto) {
+export function fetchTareasCompletadasProyecto(id_proyecto) {
 	const query = `select T.id_tarea, T.nombre_tarea from Tarea T, where T.id_proyecto = '${id_proyecto}' and T.estado_tarea = 'DONE';`;
 	return db.query(query);
 }
 
-function fetchTiempoEsProyecto(id_proyecto) {
+export function fetchTiempoEsProyecto(id_proyecto) {
 	const query = `select max(C.maximo) as tiempo_estimado from PuntosAgiles PA, Tarea_Complejidad TC, Complejidad C where PA.id_proyecto = ${id_proyecto} and PA.id_tareaComplejidad = TC.id_tareaComplejidad and TC.id_complejidad = C.id_complejidad;`;
 	return db.query(query);
 }
@@ -27,4 +27,3 @@ function fetchTiempoEsProyecto(id_proyecto) {
 // Integrantes
 // Tareas completadas
 // Tiempo estimado
-
