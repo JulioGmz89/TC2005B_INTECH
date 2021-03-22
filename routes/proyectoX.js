2e76f2b178535764e2550b90e20ba49042494872
Pau <A01701490@itesm.mx>
2021-03-22 01:58:54 -0600

    Controller y routes para Proyecto X

--- routes/proyectoX.js
+++ routes/proyectoX.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
-const proyectoXController = require('../controllers/proyectoX_controller')
+
+const proyectoXController = require('../controllers/proyectoX_controller');
+const { Router } = require('express');
 
 router.use(bodyParser.urlencoded({extended: false}));
 
-router.get('/', proyectoXController.getCasoUso);
 router.get('/PA', proyectoXController.getPA);
+router.get('/CU', proyectoXController.getCasoUso);
+router.get('/', proyectoXController.getProyectoX);
 
 module.exports = router;
