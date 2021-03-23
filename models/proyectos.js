0abe4ac075857a599fbe9b11431f4da999dd7077
Luis-ERP <leramirezp@hotmail.com>
2021-03-22 23:37:59 -0600

    modify models proyectos

--- models/proyectos.js
+++ models/proyectos.js
@@ -2,17 +2,18 @@ const db = require("./database");
 
 
 function fetchProyectosUsuario(email_usuario){
-	const query = `select  P.id_proyecto, P.nombre_proyecto, P.descripcion_proyecto, P.fechaInicio_proyecto from Proyecto P, Usuario_Proyecto UP where UP.email_usuario = "${email_usuario}" and UP.id_proyecto = P.id_proyecto;`;
+	const query = `select distinct P.id_proyecto, P.nombre_proyecto, P.descripcion_proyecto, P.fechaInicio_proyecto from Proyecto P, Usuario_Proyecto UP where UP.email_usuario = "${email_usuario}" and UP.id_proyecto = P.id_proyecto;`;
 	return db.query(query);
 }
 
 function fetchIntegrantesProyecto(id_proyecto) {
-	const query = `select UP.id_proyecto, U.email_usuario, U.nombre_usuario from Usuario U, Usuario_Proyecto UP where UP.id_proyecto = "${id_proyecto}" and UP.email_usuario = U.email_usuario;`;
+	const query = `select distinct UP.id_proyecto, U.email_usuario, U.nombre_usuario from Usuario U, Usuario_Proyecto UP where UP.id_proyecto = "${id_proyecto}" and UP.email_usuario = U.email_usuario;`;
 	return db.query(query);
 }
 
 function fetchTareasCompletadasProyecto(id_proyecto) {
-	const query = `select T.id_tarea, T.nombre_tarea from Tarea T where T.id_proyecto = "${id_proyecto}" and T.estado_tarea = "DONE";`;
+	const query = `select count(T.id_tarea) as 'tareas_completadas' from Tarea T where T.id_proyecto = "${id_proyecto}" and T.estado_tarea = "DONE";`;
+	const query2 = `select  `;
 	return db.query(query);
 }
 
@@ -22,10 +23,16 @@ function fetchTiempoEsProyecto(id_proyecto) {
 }
 
 
+function fetchNumTareasProyecto(id_proyecto){
+	const query = `select distinct count(id_tarea) as 'todas_tareas' from Tarea where id_proyecto = ${id_proyecto};`;
+	return db.query(query);
+}
+
 module.exports.fetchProyectosUsuario = fetchProyectosUsuario;
 module.exports.fetchIntegrantesProyecto = fetchIntegrantesProyecto;
 module.exports.fetchTareasCompletadasProyecto = fetchTareasCompletadasProyecto;
 module.exports.fetchTiempoEsProyecto = fetchTiempoEsProyecto;
+module.exports.fetchNumTareasProyecto = fetchNumTareasProyecto;
 
 
 
