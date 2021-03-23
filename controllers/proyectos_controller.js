25676e145608d60b3deeab8ef49bda1551824b56
Luis-ERP <leramirezp@hotmail.com>
2021-03-22 20:44:04 -0600

    proyectos controller functioning

--- controllers/proyectos_controller.js
+++ controllers/proyectos_controller.js
@@ -4,8 +4,8 @@ async function getProyectos(request, response, next) {
     const email_usuario = 'Daniel@hotmail.com';
     const proyectos = await models.fetchProyectosUsuario(email_usuario);
     var dataProyectos = [];
-    await proyectos[0].forEach( async proyecto => {
-        // console.log('1', dataProyectos.length);
+    for (let i=0; i<proyectos[0].length; i++){
+        let proyecto = proyectos[0][i];
         const integrantes = await models.fetchIntegrantesProyecto(proyecto.id_proyecto);
         const tareasCompl = await models.fetchTareasCompletadasProyecto(proyecto.id_proyecto);
         const tiempoEstim = await models.fetchTiempoEsProyecto(proyecto.id_proyecto);
@@ -16,11 +16,8 @@ async function getProyectos(request, response, next) {
             tiempoEstimado: tiempoEstim[0][0]['tiempo_estimado'],
             integrantes: integrantes[0]
         };
-        console.log(dataProyectos.length);
         dataProyectos.push(data);
-        // console.log(dataProyectos.length);
-    });
-    console.log('\n2',  dataProyectos);
+    }
     response.render('proyectos', {
         title: 'Puntos Agiles',
         proyectos: dataProyectos
