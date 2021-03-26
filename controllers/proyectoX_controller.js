2e035d04425b0a8ab2beea8c2dd891a713879c75
AdriMillares <a01705674@itesm.mx>
2021-03-26 09:14:31 -0600

    ProyectosX con información

--- controllers/proyectoX_controller.js
+++ controllers/proyectoX_controller.js
@@ -3,15 +3,31 @@ const express = require('express');
 const contextInit = require('../utils/context_manager');
 const router = express.Router();
 const path = require('path');
-const modelsProyectos = require('../models/proyectos');
+const models = require('../models/proyectos');
 router.use(express.static(path.join(__dirname, 'public')));
 
 
 exports.getProyectoX = async (request, response, next) => {
 	let context = await contextInit();
 	context.title = `Proyecto: ${request.params.id_proyecto}`;
-	const proyectoData = await modelsProyectos.fetchProyecto(request.params.id_proyecto);
-	context.proyecto = proyectoData[0][0];
+	let proyecto = await models.fetchProyecto(request.params.id_proyecto);
+	proyecto = proyecto[0][0];
+
+	const integrantes = await models.fetchIntegrantesProyecto(proyecto.id_proyecto);
+	let tareasCompl = await models.fetchTareasCompletadasProyecto(proyecto.id_proyecto);
+	let tareasTotales = await models.fetchNumTareasProyecto(proyecto.id_proyecto);
+	let tiempoEstim = await models.fetchTiempoEsProyecto(proyecto.id_proyecto);
+	tareasCompl = (tareasCompl[0].length == 0) ? 0 : tareasCompl[0].length;
+	tareasTotales = (tareasTotales[0][0]['todas_tareas'] == null) ? 0 : tareasTotales[0][0]['todas_tareas'];
+	tiempoEstim = (tiempoEstim[0][0]['tiempo_estimado'] == null) ? 0 : tiempoEstim[0][0]['tiempo_estimado'].toFixed(2);
+
+	console.log(integrantes[0]);
+	context['proyecto'] = proyecto;
+	context['tareasCompletadas'] = tareasCompl;
+	context['fechaInicio'] = proyecto.fechaInicio_proyecto;
+	context['cliente'] = proyecto.cliente_proyecto;
+	context['integrantes'] = integrantes[0];
+	context['descripcion'] = proyecto.descripcion_proyecto;
 
 	response.render('Proyecto1', context);
 };
@@ -19,7 +35,7 @@ exports.getProyectoX = async (request, response, next) => {
 
 exports.getPA = async (request, response, next) => {
 	let context = await contextInit('Puntos Ágiles');
-	const proyectoData = await modelsProyectos.fetchProyecto(request.params.id_proyecto);
+	const proyectoData = await models.fetchProyecto(request.params.id_proyecto);
 	context.proyecto = proyectoData[0][0];
 
 	response.render('PtsAgiles', context);
@@ -28,7 +44,7 @@ exports.getPA = async (request, response, next) => {
 
 exports.getCasoUso = async (request, response, next) => {
 	let context = await contextInit('Casos de Uso');
-	const proyectoData = await modelsProyectos.fetchProyecto(request.params.id_proyecto);
+	const proyectoData = await models.fetchProyecto(request.params.id_proyecto);
 	context.proyecto = proyectoData[0][0];
 	
 	response.render('CasosUso', context);
@@ -40,7 +56,7 @@ exports.getAirtable = async (request, response, next) => {
 
 	const idProyecto = request.params.id_proyecto;
 	let keys = await airtableModel.fetchKeys(idProyecto);
-	const proyectoData = await modelsProyectos.fetchProyecto(request.params.id_proyecto);
+	const proyectoData = await models.fetchProyecto(request.params.id_proyecto);
 	context.proyecto = proyectoData[0][0];
 
 	context.data = {
