d67e0cfd4e1e3b7513c81aa4730f696de7f85d84
AdriMillares <a01705674@itesm.mx>
2021-04-07 15:22:53 -0700

    Comentarios ./models

--- models/profile.js
+++ models/profile.js
@@ -1,11 +1,27 @@
+/**
+ * @brief Consultas para el manejo del perfil
+ */
+
+/**
+ * @db conexión con la base de datos
+ */
 const db = require("../utils/database");
 
-
+/**
+ * 
+ * @param {*} email_usuario 
+ * @returns Cantidad de proyectos en los que participo el usuario
+ */
 function fetchProyectos(email_usuario) {
     const query = `select count(P.id_proyecto) from Proyecto P, PuntosAgiles PA where P.id_proyecto = PA.id_proyecto and PA.email_usuario = "${email_usuario}"`;
     return db.query(query);
 }
 
+/**
+ * 
+ * @param {*} email_usuario 
+ * @returns Cantidad de proyectos creados en la empresa
+ */
 function fetchTodosProyectos(email_usuario) {
     const query = `select count(P.id_proyecto) from Proyecto P`;
     return db.query(query);
