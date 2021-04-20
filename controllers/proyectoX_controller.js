9b80d016946f8b083ecc011d0a6607818e54ba2a
Pau <A01701490@itesm.mx>
2021-04-20 09:42:56 -0500

    Avance editar proyecto

--- controllers/proyectoX_controller.js
+++ controllers/proyectoX_controller.js
@@ -13,6 +13,7 @@ exports.getProyectoX = async (request, response, next) => {
 	let context = await contextInit('Proyecto ' + request.params.id_proyecto, request);
 	let proyecto = await models.fetchProyecto(request.params.id_proyecto);
 	proyecto = proyecto[0][0];
+	const usuarios = await models.fetchTodosUsuarios();
 
 	const integrantes = await models.fetchIntegrantesProyecto(proyecto.id_proyecto);
 	let tareasCompl = await models.fetchTareasCompletadasProyecto(proyecto.id_proyecto);
@@ -29,6 +30,32 @@ exports.getProyectoX = async (request, response, next) => {
 	context['cliente'] = proyecto.cliente_proyecto;
 	context['integrantes'] = integrantes[0];
 	context['descripcion'] = proyecto.descripcion_proyecto;
+	context.usuario = usuarios[0];
 
 	response.render('Proyecto1', context);
 };
+
+exports.postEditarProyecto = async (request, response, next) => {
+
+	const id_proyecto = request.params.id_proyecto;
+	const nombreProyecto = request.body.nombreProyecto;
+    const descripcionProyecto = request.body.descripcionProyecto;
+    const clienteProyecto = request.body.clienteProyecto;
+    const integrantes = [];
+
+    for (let key in request.body) {
+        if (key.includes('id_usuario')) {
+            integrantes.push(request.body[key]);
+        }
+    }
+
+
+    const registro = await models.editProyecto(nombreProyecto, clienteProyecto, descripcionProyecto, id_proyecto);
+    // const id_proyecto = registro[0]['insertId'];
+    // integrantes.forEach(integrante => {
+    //     models.saveUserProyecto(id_proyecto, integrante).catch(error => console.log(error));
+    // });
+
+    response.redirect('/proyecto/' + id_proyecto);
+
+}
